aea6e654b3e2f3321263d4de6f25a8b48e5186ce
unknown <thavanathan.t@gmail.com>
2014-01-22 13:57:48 +0530

    * added an option to include user comments in articles. enable it in * settings. * added vconf.py - all configuration settings goes here. * bug fixes.

---
+++ me/thava/vikatan/vconf.py
@@ -0,0 +1,18 @@
+# sqlite_settings
+SQLITE_PATH = "G:\\projects\\vikatan.lite"
+
+# vikatan.com login settings
+VIKATAN_LOGIN_URL = "http://www.vikatan.com/new/"
+VIKATAN_USERNAME = "thavanathan.t@gmail.com"
+VIKATAN_PASSWORD = "zerone"
+
+# email settings
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTPS_USERNAME = "thavanathan.t@gmail.com"
+SMTPS_PASSWORD = "Saranya!23"
+EMAIL_FROM = "thavanathan.t@gmail.com"
+EMAIL_TO = "thavanathan.t@gmail.com,thavanathan.t@gmail.com"
+
+# other settings
+SHOW_COMMENTS = True
