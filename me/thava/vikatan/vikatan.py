a5e4020f47f96e6727bd2fdfb2dfad12cacfb89c
Thavanathan <thavanathan.t@gmail.com>
2014-10-15 15:15:43 +0530

    timeout optimization

--- me/thava/vikatan/vikatan.py
+++ me/thava/vikatan/vikatan.py
@@ -6,7 +6,7 @@ Created on May 2, 2013
 
 import urllib2
 import smtplib
-from bs4 import BeautifulSoup, SoupStrainer
+import time
 import sqlite3 as lite
 import argparse
 
@@ -18,51 +18,65 @@ import datetime
 import vconf
 
 
-def _init_mail_server():
-    server = smtplib.SMTP(vconf.SMTP_SERVER, vconf.SMTP_PORT)
-    server.starttls()
-    server.login(vconf.SMTPS_USERNAME, vconf.SMTPS_PASSWORD)
-    return server
+
 
 
 class Vikatan:
     content = ""
 
-    magz = ['http://www.vikatan.com/new/magazine.php?module=magazine&mid=1',
-            'http://www.vikatan.com/new/magazine.php?module=magazine&mid=2',
-            'http://www.vikatan.com/new/magazine.php?module=magazine&mid=3',
-            'http://www.vikatan.com/new/magazine.php?module=magazine&mid=6',
-            'http://www.vikatan.com/new/magazine.php?module=magazine&mid=7',
-            'http://www.vikatan.com/new/magazine.php?module=magazine&mid=17'
+    magz = [
+        'http://www.vikatan.com/new/magazine.php?module=magazine&mid=1',
+        'http://www.vikatan.com/new/magazine.php?module=magazine&mid=2',
+        'http://www.vikatan.com/new/magazine.php?module=magazine&mid=3',
+        'http://www.vikatan.com/new/magazine.php?module=magazine&mid=6',
+        'http://www.vikatan.com/new/magazine.php?module=magazine&mid=7',
+        'http://www.vikatan.com/new/magazine.php?module=magazine&mid=17'
     ]
-    namez = ['ananda vikatan',
-             'junior vikatan',
-             'aval vikatan',
-             'nanayam vikatan',
-             'motor vikatan',
-             'doctor vikatan'
+    namez = [
+        'ananda vikatan',
+        'junior vikatan',
+        'aval vikatan',
+        'nanayam vikatan',
+        'motor vikatan',
+        'doctor vikatan'
     ]
 
     def __init__(self):
+        self.email_server = None
         self.articles = []
         self.con = lite.connect(vconf.SQLITE_PATH)
         self.cur = self.con.cursor()
         self._browser = Browser()
-        self.email_server = _init_mail_server()
+
         self._login()
 
+    def _init_mail_server(self):
+        print "logging in to email server..."
+        self.email_server = smtplib.SMTP(vconf.SMTP_SERVER, vconf.SMTP_PORT)
+        self.email_server.starttls()
+        self.email_server.login(vconf.SMTPS_USERNAME, vconf.SMTPS_PASSWORD)
+
+    def _get_links(self, html):
+        links = []
+        for token in html.split('"') + html.split("'"):
+            if token.startswith("http") and "article" in token and "news" not in token \
+                    and "mid" not in token and ".css" not in token:
+                links.append(token)
+        print links
+        return set(links)
+
     def parse_vikatan(self, name, home_response):
-        # print home_response
-        for link in BeautifulSoup(home_response, parse_only=SoupStrainer('a')):
-            if "href" in getattr(link, 'attrs', {}) and link['href'].find('article.php') != -1 and link['href'].find(
-                    'news.vikatan.com') == -1:
-                url = link['href']
-                print name, url
-                self.read_article(name, url)
+        # print name, home_response
+        for url in self._get_links(home_response):
+            print name, url
+            self.read_article(name, url)
+            time.sleep(0.5)
 
         for item in self.articles:
             self.send_article(item[1], item[2], item[3])
             self.send_update(item[0])
+            time.sleep(0.5)  # to avoid google blocking connections.
+        self.articles = []
 
     def parse_args(self):
         help_text = """This program fetches articles from vikatan.com and emails the articles."""
@@ -77,12 +91,14 @@ class Vikatan:
 
     def main(self):
         for name, link in zip(self.namez, self.magz):
+            self._init_mail_server()
             ur = urllib2.urlopen(link)
             content = ur.read()
             self.parse_vikatan(name, content)
+            self.close_mail_server()
         self.cur.close()
         self.con.close()
-        self.close_mail_server()
+
 
     def _login(self):
         print "Logging into vikatan"
@@ -128,8 +144,8 @@ class Vikatan:
 
     def read_article(self, name, url):
         if self._isdup(url):
-            print "duplicate url: ", url
-            return;
+            print "Not reading duplicate url: ", url
+            return
         print "Not a dedup url"
 
         query = "insert into articles values (null, '%s', %s, %s, datetime('now', 'localtime'));" % (url, 0, 0)
@@ -140,18 +156,17 @@ class Vikatan:
         article_id = self.cur.fetchone()[0]
 
         content = self._browser.open(url).read()
-        soup = BeautifulSoup(content)
+        startindex = content.find('<div class="art_content">')
+        print startindex
+        endindex = content.find('<div class="cmt-link">')
+        print endindex
+        article = "<html><body>" + content[startindex: endindex] + "</body></html>"
 
-        mail_content = ""
-        article = soup.find("div", {"class": "art_content"})
-        mail_content += str(article)
         self.cur.execute(
             "update articles set downloaded = 1, date = datetime('now', 'localtime') where article_id=%s" % (
             article_id))
-        if vconf.SHOW_COMMENTS:
-            comments = soup.find("div", {"class": "article-comments"})
-            mail_content += str(comments)
-        self.articles.append([article_id, name, soup.title.string, mail_content])
+        title = content[content.find("<title>") + 7: content.find("</title>")]
+        self.articles.append([article_id, name, title, article])
         print len(self.articles)
 
 
